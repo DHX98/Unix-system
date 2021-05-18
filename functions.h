7ccdc56ce99a32959860b59524f2129ef2b08ac8
taylorez <taylorez@oregonstate.edu>
2021-05-17 18:48:36 -0700

    First commit of change directory, status, and function.h

---
+++ functions.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+
+void call_cd(char** array, int count);
+void call_status(char** array, int count);
+
+
+
+#endif
